09a85184026c63dc24ff4a92802465e1ac148584
bala-amypo <bala@amypo.in>
2025-12-17 04:25:58 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEnetity.java
+++ src/main/java/com/example/demo/entity/StudentEnetity.java
@@ -1,3 +1,12 @@
+
+import java.time.Localdate;
+
 public class StudentEntity{
-    
+    public StudentEntity(){
+    }
+    public StudentEntity(int id,String name,Localdate date,float cgpa){
+        this.id=id;
+        this.name=name;
+        this.date=date;
+    }
 }
